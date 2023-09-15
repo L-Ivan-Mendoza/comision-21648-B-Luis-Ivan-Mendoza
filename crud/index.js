bddff7ef2a46f02e97db4156338fbd880f305492
Ivan Mendoza <sirius.ivo@gmail.com>
2023-09-14 23:20:59 -0300

    Se agrego fecha de creacion a los posteos, se agrego estilos con bootstrap parcialmente y se intento crear el modal para eliminar post pero no se termino.

--- crud/index.js
+++ crud/index.js
@@ -5,7 +5,7 @@ const { sequelize } = require('./database');
 const e = require('cors');
 require("./src/model/Posteo");
 
-const app = express ();
+const app = express();
 
 // middleware
 app.use(express.json());
@@ -15,13 +15,13 @@ app.use(morgan('dev'));
 
 app.set("views", __dirname + "/src/views");
 
-app.use(express.static("public"));
+app.use('/public', express.static('public'));
 
 app.set("view engine", "ejs");
 
 app.use("/posteos", require("./routes/posteos.routes"));
 
 app.listen(4000, () => {
-    sequelize.sync({force: false});
+    sequelize.sync({ force: false });
     console.log("Server on, Port 4000");
 });
